8799a6a98d142d51b8d21f234376db4d97194d9c
marvim1990 <marcosvinim.cruz@gmail.com>
2024-06-21 18:42:23 +0000

    feat: Add API endpoints for creating, updating, and deleting events and spots

---
+++ src/spots/spots.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { SpotsService } from './spots.service';
+import { CreateSpotDto } from './dto/create-spot.dto';
+import { UpdateSpotDto } from './dto/update-spot.dto';
+
+@Controller('events/:eventId/spots')
+export class SpotsController {
+  constructor(private readonly spotsService: SpotsService) {}
+
+  @Post()
+  create(
+    @Param('eventId') eventId: string,
+    @Body() createSpotDto: CreateSpotDto,
+  ) {
+    return this.spotsService.create({
+      ...createSpotDto,
+      eventId,
+    });
+  }
+
+  @Get()
+  findAll(@Param('eventId') eventId: string) {
+    return this.spotsService.findAll(eventId);
+  }
+
+  @Get(':spotId')
+  findOne(@Param('id') spotId: string, @Param('eventId') eventId: string) {
+    return this.spotsService.findOne(eventId, spotId);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') spotId: string,
+    @Param('eventId') eventId: string,
+    @Body() updateSpotDto: UpdateSpotDto,
+  ) {
+    return this.spotsService.update(eventId, spotId, updateSpotDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') spotId: string, @Param('eventId') eventId: string) {
+    return this.spotsService.remove(eventId, spotId);
+  }
+}
